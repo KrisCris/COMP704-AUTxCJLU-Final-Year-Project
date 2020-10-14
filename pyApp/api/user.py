903574cd049155f07968a0a77dd9992b3266e262
Conn_Lost <hi.imhpc@outlook.com>
2020-10-14 16:16:24 +0800

    象征性的blueprint测试（x

---
+++ pyApp/api/user.py
@@ -0,0 +1,36 @@
+from flask import Blueprint, request
+
+user = Blueprint('user', __name__)
+
+
+@user.route('/')
+def user_main():
+    return 'welcome to user main page.'
+
+
+@user.route('/login', methods=['POST'])
+def login():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    # check account and login
+
+
+@user.route('/logout', methods=['GET'])
+def logout():
+    username = request.args.get('username')
+    return '{0} logged out!'.format(username)
+
+
+# LOGOUT
+
+
+@user.route('/signup', methods=['POST'])
+def signup():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    # db manipulation
+
+
+@user.route('/verify_mail', methods=['POST'])
+def verify_mail():
+    mail = request.form.get('POST')
--- pyApp/main.py
+++ pyApp/main.py
@@ -1,8 +1,22 @@
 from flask import Flask
 from util.constants import HOST, PORT, DEBUG, ENV
+from util.constants import DB_USERNAME, DB_PASSWORD, DB_ADDRESS, DB_PORT, DATABASE
+
+from db.database import db
+
+from api.user import user
 
 app = Flask(__name__)
-app.config
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://%s:%s@%s:%s/%s' % (
+    DB_USERNAME, DB_PASSWORD, DB_ADDRESS, DB_PORT, DATABASE)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.register_blueprint(user, url_prefix='/user')
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.init_app(app)
+        db.create_all()
+
     app.run(host=HOST, port=PORT, debug=DEBUG)
