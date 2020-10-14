348230b8ceaded6c26d07469155271c4712de961
Conn_Lost <hi.imhpc@outlook.com>
2020-10-14 14:01:32 +0800

    send mail

--- pyApp/util/constants.py
+++ pyApp/util/constants.py
@@ -7,9 +7,15 @@ DATABASE = 'rnd'
 CHARSET = 'utf8mb4'
 
 # EMAIL
-SENDER = 'rnd_mail@mail.connlost.online'
-SMTP_URL = 'smtp.yandex.com'
-SMTP_PORT = 465
+# SENDER = 'rnd_mail@mail.connlost.online'
+# SENDER_NAME = 'DietLens<rnd_mail@mail.connlost.online>'
+# SMTP_URL = 'smtp.yandex.com'
+# SMTP_PORT = 465
+SENDER = 'twr9738@autuni.ac.nz'
+SENDER_NAME = 'DietLens<twr9738@autuni.ac.nz>'
+SENDER_PW = ''
+SMTP_URL = 'smtp.office365.com'
+SMTP_PORT = 587
 
 # FLASK
 HOST = '0.0.0.0'
--- pyApp/util/func.py
+++ pyApp/util/func.py
@@ -1,6 +1,13 @@
+import smtplib
+import os
+
 from flask import request, jsonify, redirect
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
 
 from util.constants import REPLY_CODES
+from util.constants import SENDER, SENDER_NAME, SENDER_PW, SMTP_PORT, SMTP_URL
 
 
 def reply_json(code, data=None):
@@ -18,5 +25,58 @@ def reply_json(code, data=None):
     })
 
 
-def sendEmail(user_mail:str, content:str):
-    pass
+def __send_email(receivers: list, content: str, subject: str):
+    img_data = open('../static/logo.jpg', 'rb').read()
+    msg = MIMEMultipart()
+    msg['Subject'] = subject
+    msg['From'] = SENDER_NAME
+    msg['To'] = ','.join(receivers)
+
+    text = MIMEText('''
+        <html>
+            <body>
+                <img hidefocus="true" class="index-logo-src" src="https://i.imgur.com/hE7KCJd.png" height="96">
+                <br>
+                <div>
+                    <p>Hi,</p>
+                    <br>
+                    <div>
+                    {0}
+                    </div>
+                    <br>
+                    <p>Regards</p>
+                </div>
+            </body>
+        </html>
+    '''.format(content), 'html', 'utf-8')
+
+    msg.attach(text)
+
+    try:
+        # mail = smtplib.SMTP_SSL(SMTP_URL, SMTP_PORT)
+
+        mail = smtplib.SMTP(SMTP_URL, SMTP_PORT)
+        mail.set_debuglevel(True)
+        mail.ehlo()
+        mail.starttls()
+        mail.ehlo()
+        mail.login(SENDER, SENDER_PW)
+        mail.sendmail(SENDER_NAME, receivers, msg.as_string())
+        mail.close()
+        return 1
+    except Exception as e:
+        print('[send_email] {0}'.format(str(e)))
+        return 0
+
+
+def send_verification_code(receiver: str, code: str):
+    content = '''
+            Your verification code is: {0}.
+            This code will expire in 10 minutes.
+    '''.format(code)
+    __send_email(receivers=[receiver], content=content, subject='DietLens verification code')
+
+
+if __name__ == '__main__':
+    send_verification_code('hi.imhpc@outlook.com', 123456)
+    send_verification_code('zzwyxl@163.com', 123123)
