b4cb5ca236b11259b29fc0af61f4e35a144e93a0
Conn_Lost <hi.imhpc@outlook.com>
2020-10-24 17:18:39 +0800

    basic user logic including login and register

--- pyApp/app.py
+++ pyApp/app.py
@@ -1,8 +1,9 @@
 from flask import Flask
+
+from extensions import db
 from util.constants import HOST, PORT, DEBUG, ENV
 from util.constants import DB_USERNAME, DB_PASSWORD, DB_ADDRESS, DB_PORT, DATABASE
-
-from db.database import initDB
+from util.constants import SECRET_KEY
 
 from api.user import user
 
@@ -11,10 +12,13 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://%s:%s@%s:%s/%s' % (
     DB_USERNAME, DB_PASSWORD, DB_ADDRESS, DB_PORT, DATABASE)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = SECRET_KEY
 
 app.register_blueprint(user, url_prefix='/user')
 
 if __name__ == '__main__':
-    initDB(app)
+    with app.app_context():
+        db.init_app(app)
+        db.create_all()
 
     app.run(host=HOST, port=PORT, debug=DEBUG)
