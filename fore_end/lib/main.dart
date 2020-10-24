c8b4218d4e127456687e897995667c09ec7094a0
yun_cn <zzwyxl@163.com>
2020-10-24 16:26:55 +0800

    iconButton style basiclly done

--- fore_end/lib/main.dart
+++ fore_end/lib/main.dart
@@ -56,6 +56,8 @@ class FirstPage extends StatelessWidget {
                   color: Colors.black),
             ),
             SizedBox(height: 60),
+            MyIconButton(theme: MyTheme.blackAndWhite,
+                icon: FontAwesomeIcons.plus, text: "Text",),
             MyButton(
                 text: "Sign up",
                 fontsize: 18,
