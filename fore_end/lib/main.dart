f84e3f6ef6d39592fae1632a57001292163c4e3a
SIMON <605817494@qq.com>
2021-01-08 16:12:28 +0800

    11

--- fore_end/lib/main.dart
+++ fore_end/lib/main.dart
@@ -21,8 +21,8 @@ class MyApp extends StatelessWidget {
     this.context = context;
     this._preCacheAllImage();
     return MaterialApp(
-      //home: CoverPage(),
-      home: ComponentTestPage(),
+      home: CoverPage(),
+      // home: ComponentTestPage(),
       routes: <String, WidgetBuilder>{
         "login": (context) => Login(),
         "register": (context) => Register(),
