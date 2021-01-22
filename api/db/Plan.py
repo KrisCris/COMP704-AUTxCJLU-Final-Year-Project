39e51e27fdcb6e5fca40cde5ac860c303d078773
Conn_Lost <hi.imhpc@outlook.com>
2021-01-23 00:21:15 +0800

    db plan

--- api/db/Plan.py
+++ api/db/Plan.py
@@ -6,4 +6,28 @@ class Plan(db.Model):
 
     id = db.Column(db.INTEGER, primary_key=True, unique=True, nullable=False, autoincrement=True)
     uid = db.Column(db.INTEGER, db.ForeignKey('user.id'), nullable=False)
-    begin = db.Column(db.Ti)
+    begin = db.Column(db.INTEGER, nullable=False)
+    end = db.Column(db.INTEGER, nullable=False)
+    type = db.Column(db.INTEGER, nullable=False)
+    caloriesL = db.Column(db.FLOAT, nullable=False)
+    caloriesH = db.Column(db.FLOAT, nullable=False)
+    proteinL = db.Column(db.FLOAT, nullable=False)
+    proteinH = db.Column(db.FLOAT, nullable=False)
+
+    def __init__(self, uid, begin, end, type, caloriesL, caloriesH, proteinL, proteinH):
+        self.uid = uid
+        self.begin = begin
+        self.end = end
+        self.type = type
+        self.caloriesL = caloriesL
+        self.caloriesH = caloriesH
+        self.proteinL = proteinL
+        self.proteinH = proteinH
+
+    def add(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
--- api/db/__init__.py
+++ api/db/__init__.py
@@ -1,5 +1,6 @@
 from db.Category import Category
 from db.Food import Food
 from db.User import User
+from db.Plan import Plan
 
-__all__ = ['Category', 'User', 'Food']
+__all__ = ['Category', 'User', 'Food', 'Plan']
