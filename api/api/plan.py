bfd186406656a200bc12110675b6ddec29534236
Conn_Lost <hi.imhpc@outlook.com>
2021-01-23 13:58:16 +0800

    query_plan(ht, wt, ag, gd, pt, days, gwt, pal): JSON
    {
        "code": 1,
        "data": {
            "completedCal": 2261,
            "goalCal": 975,
            "low": true,
            "maintainCal": 2450
        },
        "msg": "Success"
    }

--- api/api/plan.py
+++ api/api/plan.py
@@ -1,24 +1,44 @@
 from flask import Blueprint, request
-
-from db import User
-from util.user import require_login
 from flasgger import swag_from
+
+from db import User, Plan
+from util.user import require_login
+from util.func import reply_json, get_current_time, get_future_time
+from util.Planer.CalsPlaner import calc_calories
 
 plan = Blueprint(name='plan', import_name=__name__)
 
+# type = 1, 2, 3., i.e. shed weight, maintain, build muscle
 
-@plan.route('set_plan', methods=['POST'])
-@require_login
-def set_plan():
-    uid = request.form.get('uid')
-    duration = request.form.get('duration')
-    height = request.form.get('height')
-    weight = request.form.get('weight')
-    age = request.form.get('age')
-    plan = request.form.get('plan')
 
-    # update age
-    user = User.getUserByID(uid)
-    user.age = age
-    User.add()
+@plan.route('query_plan', methods=['GET'])
+def query_plan():
+    # params
+    height = float(request.values.get('height'))
+    weight = float(request.values.get('weight'))
+    age = int(request.values.get('age'))
+    # male 1, female 2
+    gender = True if request.values.get('gender') == '1' else False
+    plan_type = int(request.values.get('plan'))
+    duration = int(request.values.get('duration'))
+    goal_weight = weight if plan_type == 2 or plan_type == 3 else float(request.values.get('goal_weight'))
+    physical_activity_level = float(request.values.get('pal'))
+
+    # calculation
+    result = calc_calories(
+        age=age,
+        height=height,
+        weight=weight,
+        pal=physical_activity_level,
+        time=duration,
+        goalWeight=goal_weight,
+        gender=gender,
+        type=plan_type
+    )
+
+    if result == 'unachievable':
+        return reply_json(code=-2, msg='unachievable')
+
+    return reply_json(code=1, data=result)
+
 
