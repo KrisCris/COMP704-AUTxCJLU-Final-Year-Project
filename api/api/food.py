107e26c5969ee2fa9da0d1e1ce4df6302722a3f5
Conn_Lost <hi.imhpc@outlook.com>
2021-01-31 19:00:59 +0800

    detect db search

--- api/api/food.py
+++ api/api/food.py
@@ -4,6 +4,7 @@ from flask import Blueprint, request
 from flasgger import swag_from
 
 from cv.detect import detect as food_detect
+from db.Food import Food
 from util.img import base64_to_image, fix_flutter_img_rotation_issue, crop_image_by_coords_2, img_to_b64
 from util.func import reply_json
 
@@ -22,10 +23,24 @@ def detect():
     res = food_detect(img, False)
     if res is None:
         return reply_json(-6)
-
+    import math
     res_dict = []
     for fr in res:
-        # TODO search in db
+        #  search in db
+        f = Food.searchByName(fr[5])
+        f_db = {}
+        if f:
+            pass
+            f_db['id'] = f.id
+            f_db['name'] = f.name
+            f_db['category'] = f.category
+            f_db['img'] = f.image
+            f_db['calories'] = f.calories
+            f_db['fat'] = f.fat
+            f_db['carbohydrate'] = f.carbohydrate
+            f_db['protein'] = f.protein
+            f_db['cholesterol'] = f.cholesterol
+            f_db['cellulose'] = f.cellulose
 
         # crop image based on results
         food_image = crop_image_by_coords_2(img, int(fr[0]), int(fr[1]), int(fr[2]), int(fr[3]))
@@ -35,10 +50,10 @@ def detect():
 
         # to b64
         b64_fimg = img_to_b64(food_image)
-
+        import random
         res_dict.append({
             'basic': {'img': b64_fimg, 'name': fr[5]},
-            'info': {'calories': 0, 'protain': 0, 'va': 0, 'vb': 0, 'vc': 0}
+            'info': f_db
         })
 
     return reply_json(1, data=res_dict)
