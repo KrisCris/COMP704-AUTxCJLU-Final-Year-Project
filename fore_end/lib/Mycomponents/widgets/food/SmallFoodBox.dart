dd1e62d3155a4de908a2d14689665c639a4cb198
yun_cn <zzwyxl@163.com>
2021-02-03 23:05:44 +0800

    三餐详情组件

---
+++ fore_end/lib/Mycomponents/widgets/food/SmallFoodBox.dart
@@ -0,0 +1,90 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
+import 'package:fore_end/MyTool/Food.dart';
+import 'package:fore_end/MyTool/util/MyTheme.dart';
+
+class SmallFoodBox extends StatefulWidget {
+  double pictureSize;
+  TextStyle style;
+  TextStyle figureStyle;
+  Food food;
+  SmallFoodBox(
+      {@required this.food, this.pictureSize = 40, TextStyle style, Key key})
+      : assert(food != null),
+        super(key: key) {
+    this.style = style ??
+        TextStyle(
+            fontFamily: "Futura",
+            fontSize: 15,
+            decoration: TextDecoration.none,
+            color: MyTheme.convert(ThemeColorName.NormalText));
+    this.figureStyle = TextStyle(
+        fontFamily: "Futura",
+        fontSize: this.style.fontSize * 0.8,
+        decoration: TextDecoration.none,
+        color: MyTheme.convert(ThemeColorName.NormalText));
+  }
+  @override
+  State<StatefulWidget> createState() {
+    return SmallFoodBoxState();
+  }
+}
+
+class SmallFoodBoxState extends State<SmallFoodBox> {
+  bool showDetail = false;
+  @override
+  Widget build(BuildContext context) {
+    Image img = widget.food.picture == null?Image.asset(
+      Food.defaultPicturePath,
+      fit: BoxFit.cover,
+      width: widget.pictureSize,
+      height: widget.pictureSize,
+    ):Image.memory(
+      base64Decode(widget.food.picture),
+      fit: BoxFit.cover,
+      width: widget.pictureSize,
+      height: widget.pictureSize,
+    );
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          showDetail = !showDetail;
+        });
+      },
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          ClipRRect(
+            child: img
+          ),
+          SizedBox(width: 5),
+          AnimatedCrossFade(
+            firstChild: SizedBox(width: 0),
+            secondChild: Container(
+              height: widget.pictureSize,
+              width: 80,
+              child: ListView(
+                scrollDirection: Axis.vertical,
+                padding: EdgeInsets.only(top: 0),
+                children: [
+                  Text(widget.food.name, style: widget.style),
+                  Text("Calorie: " + widget.food.calorie.toString() + "Kcal",
+                      style: widget.figureStyle),
+                  Text("Protein: " + widget.food.protein.toString() + "mg",
+                      style: widget.figureStyle),
+                ],
+              )
+            ),
+            crossFadeState: this.showDetail
+                ? CrossFadeState.showSecond
+                : CrossFadeState.showFirst,
+            duration: Duration(milliseconds: 100),
+          )
+        ],
+      ),
+    );
+  }
+}
