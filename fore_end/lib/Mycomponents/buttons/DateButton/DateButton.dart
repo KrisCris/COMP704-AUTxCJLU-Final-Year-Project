d867ea46f04aecfa6483900d5b67a4f951e25e26
yun_cn <zzwyxl@163.com>
2021-02-04 18:56:20 +0800

    添加注释

--- fore_end/lib/Mycomponents/buttons/DateButton/DateButton.dart
+++ fore_end/lib/Mycomponents/buttons/DateButton/DateButton.dart
@@ -5,13 +5,21 @@ import 'package:fore_end/MyTool/util/ScreenTool.dart';
 import 'package:fore_end/interface/Valueable.dart';
 
 class DateButton extends StatefulWidget with ValueableWidgetMixIn<int> {
+  ///宽度
   double width;
+  ///高度
   double height;
+  ///文字部分距离按钮左右边界的距离
   double paddingHorizontal;
+  ///时间选择范围的最后一天
   DateTime lastTime;
+  ///时间选择范围的第一天
   DateTime beginTime;
+  ///文字样式,不设置则使用默认配置
   TextStyle style;
+  ///按钮样式，不设置则使用默认配置
   BoxDecoration decoration;
+  ///当日期变化时触发的回调函数
   Function onChangeDate;
   DateButton(
       {double width,
@@ -26,16 +34,21 @@ class DateButton extends StatefulWidget with ValueableWidgetMixIn<int> {
     this.width = ScreenTool.partOfScreenWidth(width);
     this.height = ScreenTool.partOfScreenHeight(height);
     this.widgetValue = new ValueNotifier<int>(DateTime.now().millisecondsSinceEpoch);
+    //style为null,则使用后面的默认项目，否则使用style
     this.style = style ??
         TextStyle(
             fontFamily: "Futura",
             fontSize: 12,
             decoration: TextDecoration.none,
             color: MyTheme.convert(ThemeColorName.NormalText));
+
+    //decoration为null,则使用后面的默认项目,否则使用decoration
     this.decoration = decoration ??
         BoxDecoration(
             color: MyTheme.convert(ThemeColorName.TransparentShadow),
             borderRadius: BorderRadius.circular(5));
+
+    //开始时间和结束时间若为null，使用默认项目
     this.beginTime = beginTime ?? DateTime(2021, 1, 1);
     this.lastTime = lastTime ?? DateTime(2021, 12, 1);
   }
@@ -100,6 +113,7 @@ class DateButtonState extends State<DateButton> with ValueableStateMixIn {
     setState(() {});
   }
 
+  ///将时间戳转为日期字符串格式
   String convertToStr() {
     DateTime dt = DateTime.fromMillisecondsSinceEpoch(widget.widgetValue.value);
     return dt.year.toString() +
