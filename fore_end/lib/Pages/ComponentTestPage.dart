c94692b711ee602c75fabd9e04abcc39d596cb8f
yun_cn <zzwyxl@163.com>
2021-02-04 17:08:41 +0800

    Auto stash before merge of "develop" and "origin/develop"

--- fore_end/lib/Pages/ComponentTestPage.dart
+++ fore_end/lib/Pages/ComponentTestPage.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fore_end/MyTool/Food.dart';
+import 'package:fore_end/MyTool/Meal.dart';
 import 'package:fore_end/MyTool/util/MyTheme.dart';
 import 'package:fore_end/MyTool/util/ScreenTool.dart';
-import 'package:fore_end/Mycomponents/widgets/MealList.dart';
+import 'package:fore_end/Mycomponents/buttons/DateButton/DateButton.dart';
+import 'package:fore_end/Mycomponents/buttons/DateButton/DateSelect.dart';
+import 'package:fore_end/Mycomponents/widgets/food/DetailedMealList.dart';
+import 'package:fore_end/Mycomponents/widgets/food/SmallFoodBox.dart';
+import 'file:///E:/phpstudy_pro/WWW/Food-detection-based-mobile-diet-keeper/fore_end/lib/Mycomponents/widgets/food/MealList.dart';
 import 'dart:math' as math;
-
 import 'package:material_floating_search_bar/material_floating_search_bar.dart';
 
 class ComponentTestPage extends StatefulWidget {
@@ -33,100 +38,15 @@ class ComponentTestState extends State<ComponentTestPage>
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // This is handled by the search bar itself.
-      resizeToAvoidBottomInset: false,
-      body: Stack(
-        fit: StackFit.expand,
-        children: [
-          ClipRect(
-            child: Container(
-              width: ScreenTool.partOfScreenWidth(1),
-              height: ScreenTool.partOfScreenHeight(1),
-              color: Color(0xFF172632),
-            ),
-          ),
-
-          buildFloatingSearchBar(),
-          SizedBox(height: 111,),
-          Container(
-            margin: EdgeInsets.only(top: 200,left: 15,right: 15,bottom: 200),
-            width: 10,
-            height: 10,
-            decoration: BoxDecoration(
-              border: Border.all(),
-            ),
-            child: Text("Test SearchBar"),
-          ),
-        ],
-      ),
+    return Container(
+      color: Colors.green,
+      child: Center(
+        child: DetailedMealList(
+          meal: Meal(mealName: "breakfast",encoded:"Rice-237-12,Pork-419-58,lettus-64-1"),
+          width: 0.9,
+          dragAreaHeight: 50,
+        )
+      )
     );
   }
-
-  Widget buildFloatingSearchBar() {
-    final isPortrait = MediaQuery.of(context).orientation == Orientation.portrait;
-
-    return FloatingSearchBar(
-      backgroundColor: Colors.blue,
-      hint: 'Search...',
-      hintStyle: TextStyle(color: Colors.white,fontSize: 16),
-      accentColor: Colors.white,
-      scrollPadding: const EdgeInsets.only(top: 16, bottom: 56),
-      transitionDuration: const Duration(milliseconds: 800),
-      transitionCurve: Curves.easeInOut,
-      automaticallyImplyDrawerHamburger: true,
-      physics: const BouncingScrollPhysics(),
-      axisAlignment: isPortrait ? 0.0 : -1.0,
-      openAxisAlignment: 0.0,
-      maxWidth: isPortrait ? 600 : 500,
-      debounceDelay: const Duration(milliseconds: 500),
-      onQueryChanged: (query) {
-        // Call your model, bloc, controller here.
-      },
-      // Specify a custom transition to be used for
-      // animating between opened and closed stated.
-      transition: CircularFloatingSearchBarTransition(),
-      actions: [
-        FloatingSearchBarAction(
-          showIfOpened: false,
-          child: CircularButton(
-            icon: const Icon(Icons.place),
-            onPressed: () {},
-          ),
-        ),
-        FloatingSearchBarAction.searchToClear(
-          showIfClosed: false,
-        ),
-      ],
-      builder: (context, transition) {
-        return ClipRRect(
-          borderRadius: BorderRadius.circular(8),
-          child: Material(
-            color: Colors.white,
-            elevation: 4.0,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: Colors.accents.map((color) {
-                return Container(height: 30, color: color);
-              }).toList(),
-            ),
-          ),
-        );
-      },
-    );
-  }
-
-
-// @override
-  // Widget build(BuildContext context) {
-  //  return  ListView(
-  //    scrollDirection: Axis.vertical,
-  //    children: <Widget>[
-  //      SizedBox(height: 50,),
-  //
-  //    ],
-  //
-  //  );
-  //
-  // }
 }
