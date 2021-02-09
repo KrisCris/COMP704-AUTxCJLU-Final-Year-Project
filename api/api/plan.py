00626395944edac932167cc6361ccb4b11bae5d1
Conn_Lost <hi.imhpc@outlook.com>
2021-02-09 18:19:47 +0800

    [API_MOD] Migrate consume_food to Food

--- api/api/plan.py
+++ api/api/plan.py
@@ -173,28 +173,6 @@ def get_plan():
         return reply_json(-6)
 
 
-@plan.route('consume_foods', methods=['POST'])
-@require_login
-def consume_foods():
-    uid = request.form.get('uid')
-    pid = request.form.get('pid')
-    # 1 = breakfast, 2 = lunch, 3 = dinner
-    type = request.form.get('type')
-    # a list that contains all the food and its corresponding info including proteins, calories, names.
-    foods_info = request.form.get('foods_info')
-    # TODO check the food_info's type
-    p = Plan.getPlanByID(pid)
-
-    day = get_relative_days(p.begin, get_current_time()) + 1
-    for food_info in foods_info:
-        f = DailyConsumption(
-            uid=uid, pid=pid, type=type, fid=food_info[0], day=day,
-            name=food_info[1], calories=foods_info[2], protein=foods_info[3]
-        )
-        f.add()
-    return reply_json(1)
-
-
 @plan.route('update_body_info', methods=['POST'])
 @require_login
 def update_body_info():
