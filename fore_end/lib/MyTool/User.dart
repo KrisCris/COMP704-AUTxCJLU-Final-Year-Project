16302b39a3b3c6ed4a0d1b7a5b38a40e9308f5a9
yun_cn <zzwyxl@163.com>
2021-02-21 22:28:03 +0800

    如果延期，请求服务器计算出可能延期的天数

--- fore_end/lib/MyTool/User.dart
+++ fore_end/lib/MyTool/User.dart
@@ -42,6 +42,7 @@ class User {
   bool _shouldUpdateWeight;
   bool _pastDeadline;
   bool _isOffline;
+  int _calculatedDelayTime;
 
   ///下面是Simon新加的mealData属性，用来存放用户的一日三餐信息。
   ///计划是：每次启动程序时，先去服务器/数据库获取最新的用户添加的食物数据，然后更新本地的数据。
@@ -75,6 +76,7 @@ class User {
     this._registerDate = registerDate;
     this._needGuide = needGuide;
     this._isOffline = offline;
+
     ///下面是Simon新加的mealData属性
     this.meals = new ValueNotifier<List<Meal>>([]);
     this.meals.value = [
@@ -96,12 +98,14 @@ class User {
       }
     }
   }
+
   ///从本地文件读取用户信息
   static User getInstance() {
     if (User._instance == null) {
       SharedPreferences pre = LocalDataManager.pre;
-      if(pre == null){
-        print("Local Data Manager has not been initialized yet! User info getting failed!");
+      if (pre == null) {
+        print(
+            "Local Data Manager has not been initialized yet! User info getting failed!");
         return null;
       }
       User._instance = User._internal(
@@ -121,15 +125,16 @@ class User {
     }
     return User._instance;
   }
-  
-  static bool isInit(){
+
+  static bool isInit() {
     return User._instance != null;
   }
+
   ///与服务器上的用户数据同步
   Future<int> synchronize() async {
     Response res =
-    await Requests.getBasicInfo({'uid': this._uid, 'token': this._token});
-    if(res == null){
+        await Requests.getBasicInfo({'uid': this._uid, 'token': this._token});
+    if (res == null) {
       return 5;
     }
 
@@ -139,56 +144,53 @@ class User {
       this._userName = res.data['data']['nickname'];
       this._avatar = res.data['data']['avatar'];
       this._email = res.data['data']['email'];
-      this._bodyHeight = res.data['data']['height']/100;
+      this._bodyHeight = res.data['data']['height'] / 100;
       this._bodyWeight = res.data['data']['weight'];
       this._needGuide = res.data['data']['needGuide'];
-      this._registerDate =res.data['data']['register_date'];
+      this._registerDate = res.data['data']['register_date'];
 
       DateTime nowDay = DateTime.now();
-      nowDay = DateTime(nowDay.year,nowDay.month,nowDay.day);
+      nowDay = DateTime(nowDay.year, nowDay.month, nowDay.day);
       res = await Requests.historyMeal({
-        "uid":this._uid,
-        "token":this._token,
-        "begin":nowDay.millisecondsSinceEpoch/1000,
-        "end":nowDay.add(Duration(days: 1)).millisecondsSinceEpoch/1000 - 1
+        "uid": this._uid,
+        "token": this._token,
+        "begin": nowDay.millisecondsSinceEpoch / 1000,
+        "end": nowDay.add(Duration(days: 1)).millisecondsSinceEpoch / 1000 - 1
       });
-      if(res != null){
-        if(res.data['code'] == 1){
-          for(Map m in res.data['data']['b']){
+      if (res != null) {
+        if (res.data['code'] == 1) {
+          for (Map m in res.data['data']['b']) {
             this.meals.value[0].foods = [];
-            this.meals.value[0].time = m['time']*1000;
+            this.meals.value[0].time = m['time'] * 1000;
             this.meals.value[0].addFood(new Food(
                 name: m['name'],
                 id: m['fid'],
                 calorie: m['calories'],
                 picture: m['img'],
                 protein: m['protein'],
-                weight: (m['weight'] as double).floor()
-            ));
+                weight: (m['weight'] as double).floor()));
           }
-          for(Map m in res.data['data']['l']){
+          for (Map m in res.data['data']['l']) {
             this.meals.value[1].foods = [];
-            this.meals.value[1].time = m['time']*1000;
+            this.meals.value[1].time = m['time'] * 1000;
             this.meals.value[1].addFood(new Food(
                 name: m['name'],
                 id: m['fid'],
                 calorie: m['calories'],
                 picture: m['img'],
                 protein: m['protein'],
-                weight: (m['weight'] as double).floor()
-            ));
+                weight: (m['weight'] as double).floor()));
           }
-          for(Map m in res.data['data']['d']){
+          for (Map m in res.data['data']['d']) {
             this.meals.value[2].foods = [];
-            this.meals.value[2].time = m['time']*1000;
+            this.meals.value[2].time = m['time'] * 1000;
             this.meals.value[2].addFood(new Food(
                 name: m['name'],
                 id: m['fid'],
                 calorie: m['calories'],
                 picture: m['img'],
                 protein: m['protein'],
-                weight: (m['weight'] as double).floor()
-            ));
+                weight: (m['weight'] as double).floor()));
           }
         }
       }
@@ -205,22 +207,28 @@ class User {
             goalWeight: res.data['data']['goal'],
             extendDays: res.data['data']['ext'] ?? 0,
             dailyCaloriesLowerLimit:
-            NumUtil.getNumByValueDouble(res.data['data']['cl'], 1),
+                NumUtil.getNumByValueDouble(res.data['data']['cl'], 1),
             dailyCaloriesUpperLimit:
-            NumUtil.getNumByValueDouble(res.data['data']['ch'], 1),
+                NumUtil.getNumByValueDouble(res.data['data']['ch'], 1),
             dailyProteinLowerLimit:
-            NumUtil.getNumByValueDouble(res.data['data']['pl'], 1),
+                NumUtil.getNumByValueDouble(res.data['data']['pl'], 1),
             dailyProteinUpperLimit:
-            NumUtil.getNumByValueDouble(res.data['data']['ph'], 1));
+                NumUtil.getNumByValueDouble(res.data['data']['ph'], 1));
       }
-      this._pastDeadline = DateTime.now().millisecondsSinceEpoch > (this._plan.endTime*1000 + 3600*24* this._plan.extendDays*1000);
+      this._pastDeadline = DateTime.now().millisecondsSinceEpoch >
+          (this._plan.endTime * 1000 +
+              3600 * 24 * this._plan.extendDays * 1000);
+      if (this.pastDeadline) {
+        res = await Requests.calculateDelayTime(
+            {"uid": this._uid, "token": this._token, "pid": this._plan.id});
+        if (res != null && res.data['code'] == 1) {
+          this._calculatedDelayTime = res.data['data']['ext'];
+        }
+      }
       this.save();
-      res = await Requests.shouldUpdateWeight({
-        "uid":this._uid,
-        "token":this._token,
-        "pid":this._plan.id
-      });
-      if(res != null && res.data['code'] == 1){
+      res = await Requests.shouldUpdateWeight(
+          {"uid": this._uid, "token": this._token, "pid": this._plan.id});
+      if (res != null && res.data['code'] == 1) {
         this._shouldUpdateWeight = res.data['data']['shouldUpdate'];
       }
       return 4;
@@ -228,51 +236,56 @@ class User {
       return 3;
     }
   }
+
   //meal相关
-  bool hasMealName(String s){
-    for(Meal m in this.meals.value){
-      if(s == m.mealName){
+  bool hasMealName(String s) {
+    for (Meal m in this.meals.value) {
+      if (s == m.mealName) {
         return true;
       }
     }
     return false;
   }
-  Meal getMealByName(String s){
-    for(Meal m in this.meals.value){
-      if(s == m.mealName){
+
+  Meal getMealByName(String s) {
+    for (Meal m in this.meals.value) {
+      if (s == m.mealName) {
         return m;
       }
     }
     return null;
   }
-  void saveMeal(){
+
+  void saveMeal() {
     this.meals.value.forEach((element) {
       element.save();
     });
   }
-  void readLocalMeal(){
+
+  void readLocalMeal() {
     DateTime nowDay = DateTime.now();
-    nowDay = DateTime(nowDay.year,nowDay.month,nowDay.day);
-    for(Meal m in this.meals.value){
+    nowDay = DateTime(nowDay.year, nowDay.month, nowDay.day);
+    for (Meal m in this.meals.value) {
       m.read();
       //m.delete();
-      if(m.time == null)continue;
+      if (m.time == null) continue;
 
-      if(m.time < nowDay.millisecondsSinceEpoch ||
-          m.time >= nowDay.add(Duration(days: 1)).millisecondsSinceEpoch){
+      if (m.time < nowDay.millisecondsSinceEpoch ||
+          m.time >= nowDay.add(Duration(days: 1)).millisecondsSinceEpoch) {
         m.foods = [];
       }
     }
   }
-  void refreshMeal(){
-    for(Meal m in meals.value){
+
+  void refreshMeal() {
+    for (Meal m in meals.value) {
       State st = m.key.currentState;
-      if(st != null && st.mounted){
-        st.setState(() {
-        });
+      if (st != null && st.mounted) {
+        st.setState(() {});
       }
     }
   }
+
   //plan相关
   void setPlan(res) {
     this._plan = new Plan(
@@ -287,25 +300,35 @@ class User {
         dailyProteinUpperLimit: res.data['data']['ph']);
     this._plan.save();
   }
-  void solvePastDeadline(BuildContext context, int days){
+
+  void solvePastDeadline(BuildContext context) {
     //TODO: 获取后端计算出来的时间
-    if(this._pastDeadline){
+    if (this._pastDeadline) {
       WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
         showDialog<bool>(
           context: context,
           builder: (BuildContext context) {
-            return ExtendTimeHint(extendDays: days,onAcceptDelay: (){
-              Navigator.of(context).pop();
-            },);
+            return ExtendTimeHint(
+              extendDays: this._calculatedDelayTime,
+              onAcceptDelay: () {
+                Navigator.of(context).pop();
+              },
+              onFinishPlan: (){
+                Requests.finishPlan({
+
+                });
+              },
+            );
           },
-        ).then((value){
+        ).then((value) {
           this.solveUpdateWeight(context);
         });
       });
     }
   }
-  void solveUpdateWeight(BuildContext context){
-    if(this._shouldUpdateWeight){
+
+  void solveUpdateWeight(BuildContext context) {
+    if (this._shouldUpdateWeight) {
       WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
         this._shouldUpdateWeight = false;
         showDialog<bool>(
@@ -316,21 +339,20 @@ class User {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  DotColumn(
-                      borderRadius: 5,
-                      children: [
-                        SizedBox(height: ScreenTool.partOfScreenHeight(0.05)),
-                        Container(
-                          child: Text(
-                            "1 week had passed since your last body weight updating. It's time to update!",style: TextStyle(
-                              fontFamily: "Futura",
-                              fontSize: 15,
-                              color: MyTheme.convert(ThemeColorName.NormalText)
-                          ),),
-                          width: ScreenTool.partOfScreenWidth(0.6),
-                        ),
-                        SizedBox(height: ScreenTool.partOfScreenHeight(0.05)),
-                      ])
+                  DotColumn(borderRadius: 5, children: [
+                    SizedBox(height: ScreenTool.partOfScreenHeight(0.05)),
+                    Container(
+                      child: Text(
+                        "1 week had passed since your last body weight updating. It's time to update!",
+                        style: TextStyle(
+                            fontFamily: "Futura",
+                            fontSize: 15,
+                            color: MyTheme.convert(ThemeColorName.NormalText)),
+                      ),
+                      width: ScreenTool.partOfScreenWidth(0.6),
+                    ),
+                    SizedBox(height: ScreenTool.partOfScreenHeight(0.05)),
+                  ])
                 ],
               ),
             );
@@ -340,36 +362,37 @@ class User {
     }
   }
 
-  int getTodayCaloriesIntake(){
+  int getTodayCaloriesIntake() {
     int cal = 0;
-    for(Meal m in meals.value){
+    for (Meal m in meals.value) {
       cal += m.calculateTotalCalories();
     }
     return cal;
   }
-  int getTodayProteinIntake(){
+
+  int getTodayProteinIntake() {
     int pro = 0;
-    for(Meal m in meals.value){
+    for (Meal m in meals.value) {
       pro += m.calculateTotalCalories();
     }
     return pro;
   }
-  Future<int> updateBodyData({double weight, double height,BuildContext context}) async {
+
+  Future<int> updateBodyData(
+      {double weight, double height, BuildContext context}) async {
     Response res = await Requests.updateBody({
-      "uid":_uid,
-      "token":_token,
-      "height":height,
-      "weight":weight,
+      "uid": _uid,
+      "token": _token,
+      "height": height,
+      "weight": weight,
     });
-    if(res == null){
+    if (res == null) {
       return -1;
     }
-    if(res.data['code'] == 1){
-      if(weight != null)this._bodyWeight = weight;
-      if(height != null)this._bodyHeight = height/100;
-      if(res.data['data'] == null){
-
-      }
+    if (res.data['code'] == 1) {
+      if (weight != null) this._bodyWeight = weight;
+      if (height != null) this._bodyHeight = height / 100;
+      if (res.data['data'] == null) {}
       this._plan = new Plan(
           id: res.data['data']['pid'],
           startTime: res.data['data']['begin'],
@@ -377,25 +400,27 @@ class User {
           planType: res.data['data']['type'],
           goalWeight: res.data['data']['goal'],
           dailyCaloriesLowerLimit:
-          NumUtil.getNumByValueDouble(res.data['data']['cl'], 1),
+              NumUtil.getNumByValueDouble(res.data['data']['cl'], 1),
           dailyCaloriesUpperLimit:
-          NumUtil.getNumByValueDouble(res.data['data']['ch'], 1),
+              NumUtil.getNumByValueDouble(res.data['data']['ch'], 1),
           dailyProteinLowerLimit:
-          NumUtil.getNumByValueDouble(res.data['data']['pl'], 1),
+              NumUtil.getNumByValueDouble(res.data['data']['pl'], 1),
           dailyProteinUpperLimit:
-          NumUtil.getNumByValueDouble(res.data['data']['ph'], 1));
+              NumUtil.getNumByValueDouble(res.data['data']['ph'], 1));
       return 1;
-    }else if(res.data['code'] == -2){
-      this.solvePastDeadline(context,res.data['data']['recommend_ext']);
+    } else if (res.data['code'] == -2) {
+      this._calculatedDelayTime = res.data['data']['recommend_ext'];
+      this.solvePastDeadline(context);
       return -2;
     }
   }
-  double getRemainWeight(){
+
+  double getRemainWeight() {
     double result = this._bodyWeight - this._plan.goalWeight;
-    if(result < 0)result = 0;
+    if (result < 0) result = 0;
     return result;
   }
-  
+
   void save() {
     SharedPreferences pre = LocalDataManager.pre;
     pre.setString("token", _token);
@@ -432,7 +457,7 @@ class User {
     });
     Plan.removeLocal();
   }
-  
+
   bool get isOffline => _isOffline;
   double get bodyWeight => _bodyWeight;
   String get token => _token;
@@ -450,36 +475,47 @@ class User {
   set pastDeadline(bool value) {
     _pastDeadline = value;
   }
-  bool get shouldUpdateWeight{
+
+  bool get shouldUpdateWeight {
     return this._shouldUpdateWeight;
   }
-  set shouldUpdateWeight(bool value){
+
+  set shouldUpdateWeight(bool value) {
     this._shouldUpdateWeight = value;
   }
+
   set isOffline(bool value) {
     _isOffline = value;
   }
+
   set token(String value) {
     _token = value;
   }
+
   set uid(int value) {
     _uid = value;
   }
+
   set age(int value) {
     _age = value;
   }
+
   set gender(int value) {
     _gender = value;
   }
+
   set userName(String value) {
     _userName = value;
   }
+
   set email(String value) {
     _email = value;
   }
+
   set avatar(String value) {
     _avatar = value;
   }
+
   Image getAvatar(double width, double height) {
     return Image.memory(
       base64Decode(this._avatar),
@@ -488,25 +524,25 @@ class User {
       fit: BoxFit.cover,
     );
   }
+
   Uint8List getAvatarBin() {
     return base64Decode(this._avatar);
   }
+
   Icon genderIcon() {
     return User.genderIcons[this._gender];
   }
 }
 
-class BodyChangeLog{
+class BodyChangeLog {
   int time;
   int weight;
   int height;
 
   BodyChangeLog({this.time, this.weight, this.height});
 
-  String getTime(){
-    return DateUtil.formatDate(
-        DateTime.fromMillisecondsSinceEpoch(this.time),
-        format: "MM-dd"
-    );
+  String getTime() {
+    return DateUtil.formatDate(DateTime.fromMillisecondsSinceEpoch(this.time),
+        format: "MM-dd");
   }
 }
--- fore_end/lib/MyTool/util/Req.dart
+++ fore_end/lib/MyTool/util/Req.dart
@@ -190,6 +190,9 @@ class Requests {
   static Future<Response> delayPlan(data) async {
     return _postRequest("delayPlan", data, "/plan/extend_plan", "extend_plan接口", null);
   }
+  static Future<Response> calculateDelayTime(data) async {
+    return _postRequest("calculateDelayTime", data, "/plan/estimate_extension", "estimate_extension接口", null);
+  }
   static Future<Response> getWeightTrend(data) async {
     return _postRequest("get_weight_trend", data, "/plan/get_weight_trend", "get_weight_trend接口", null);
   }
