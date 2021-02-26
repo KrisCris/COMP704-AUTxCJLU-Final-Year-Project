0dfb13a54e92322167bd523df4ad9f3365da3e35
yun_cn <zzwyxl@163.com>
2021-02-26 14:29:37 +0800

    历史三餐已经可以展示离线数据

--- fore_end/lib/Pages/detail/DetailMealPage.dart
+++ fore_end/lib/Pages/detail/DetailMealPage.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_picker/flutter_picker.dart';
@@ -5,6 +7,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:fore_end/MyTool/Food.dart';
 import 'package:fore_end/MyTool/Meal.dart';
 import 'package:fore_end/MyTool/User.dart';
+import 'package:fore_end/MyTool/util/LocalDataManager.dart';
 import 'package:fore_end/MyTool/util/MyTheme.dart';
 import 'package:fore_end/MyTool/util/Req.dart';
 import 'package:fore_end/MyTool/util/ScreenTool.dart';
@@ -12,6 +15,7 @@ import 'package:fore_end/Mycomponents/buttons/CustomIconButton.dart';
 import 'package:fore_end/Mycomponents/buttons/DateButton/DateButton.dart';
 import 'package:fore_end/Mycomponents/text/TitleText.dart';
 import 'package:fore_end/Mycomponents/widgets/food/DetailedMealList.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class DetailMealPage extends StatefulWidget{
   DateTime mealTime;
@@ -25,19 +29,65 @@ class DetailMealPage extends StatefulWidget{
 
 class DetailMealPageState extends State<DetailMealPage> {
   List<Meal> meal;
-
+  User u;
+  DateTime beginTime;
+  DateTime endTime;
+  Map<String,dynamic> localMealMap;
   @override
   void initState() {
+    this.u = User.getInstance();
+    DateTime now = DateTime.now();
+    this.endTime = DateTime(now.year,now.month,now.day);
+    this.beginTime = this.endTime.add(Duration(days: -1*u.registerTime()));
+    if(u.isOffline){
+      SharedPreferences pre = LocalDataManager.pre;
+      int bg = pre.getInt("localBeginTime");
+      int ed = pre.getInt("localEndTime");
+      String json = pre.getString("localHistoryMeals");
+      this.localMealMap = jsonDecode(json);
+      if(bg != null){
+        this.beginTime = DateTime.fromMillisecondsSinceEpoch(bg);
+      }
+      if(ed != null){
+        this.endTime = DateTime.fromMillisecondsSinceEpoch(ed);
+      }
+    }
     this.judgeDate();
     super.initState();
   }
   void judgeDate({DateTime time}){
+    if(this.u.isOffline){
+      DateTime settingDay = time ?? DateTime(this.endTime.year,this.endTime.month,this.endTime.day);
+      String stringTime = settingDay.millisecondsSinceEpoch.toString();
+      this.meal = [
+        Meal(mealName: "breakfast"),
+        Meal(mealName: "lunch"),
+        Meal(mealName: "dinner")
+      ];
+      if(this.localMealMap.containsKey(stringTime)){
+        for(Map m in this.localMealMap[stringTime]){
+          if(m['type'] >= 1 && m['type'] <= 3){
+            this.meal[m['type']-1].addFood(new Food(
+              calorie: m['calories'],
+              id: m['fid'],
+              picture: m['img'],
+              name: m['name'],
+              protein: m['protein'],
+              weight: (m['weight'] as double).floor()
+            ));
+          }
+        }
+      }
+      if(mounted){
+        setState(() {});
+      }
+      return;
+    }
     time = time ?? widget.mealTime;
+    DateTime settingDay = DateTime(time.year,time.month,time.day);
     DateTime nowCurrent = DateTime.now();
     DateTime today = DateTime(nowCurrent.year,nowCurrent.month,nowCurrent.day);
-    DateTime settingDay = DateTime(time.year,time.month,time.day);
     if(settingDay.compareTo(today) == 0){
-      User u = User.getInstance();
       this.meal = u.meals.value;
       if(mounted){
         setState(() {});
@@ -120,8 +170,8 @@ class DetailMealPageState extends State<DetailMealPage> {
             children: [DateSelect(
               width: 0.5,
               height: 40,
-              beginTime: DateTime(2021,1,1),
-              lastTime: DateTime.now(),
+              beginTime: this.beginTime,
+              lastTime: this.endTime,
               onChangeDate: (int newDate){
                 this.judgeDate(time:DateTime.fromMillisecondsSinceEpoch(newDate));
               },
