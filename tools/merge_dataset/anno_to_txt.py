ae4d779fdb769fc2158975da6ed4601ed4ce69fc
Paul <yingheting@gmail.com>
2021-03-01 11:57:01 +0800

    标注越界处理

--- tools/merge_dataset/anno_to_txt.py
+++ tools/merge_dataset/anno_to_txt.py
@@ -91,6 +91,13 @@ def convert_annotation(image_id):
         xmlbox = obj.find('bndbox')
         b = (float(xmlbox.find('xmin').text), float(xmlbox.find('xmax').text), float(xmlbox.find('ymin').text),
              float(xmlbox.find('ymax').text))
+        b1, b2, b3, b4 = b
+        # 标注越界修正
+        if b2 > w:
+            b2 = w
+        if b4 > h:
+            b4 = h
+        b = (b1, b2, b3, b4)
         bb = convert((w, h), b)
         out_file.write(str(cls_id) + " " + " ".join([str(a) for a in bb]) + '\n')
 
