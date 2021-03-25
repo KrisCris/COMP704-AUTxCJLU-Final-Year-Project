2cacfd32d4971ae358d49d7bcb52c7ee31245050
Conn_Lost <hi.imhpc@outlook.com>
2021-03-25 16:23:05 +0800

    [DB] ADDED CN NAME FOR SOME DATA, REMOVED UNUSED ITEMS

--- api/db/Category.py
+++ api/db/Category.py
@@ -6,6 +6,8 @@ class Category(db.Model):
 
     id = db.Column(db.INTEGER, primary_key=True, unique=True, nullable=False, autoincrement=True)
     name = db.Column(db.VARCHAR(256), nullable=False)
+    cnName = db.Column(db.VARCHAR(256))
 
-    def __init__(self, name):
+    def __init__(self, name, cnName=None):
+        self.cnName = cnName
         self.name = name
--- api/db/Food.py
+++ api/db/Food.py
@@ -6,19 +6,20 @@ class Food(db.Model):
 
     id = db.Column(db.INTEGER, primary_key=True, unique=True, nullable=False, autoincrement=True)
     name = db.Column(db.VARCHAR(255), unique=True, nullable=False, comment='food name')
+    cnName = db.Column(db.VARCHAR(255))
     category = db.Column(db.ForeignKey('category.id'), index=True, comment='food category, details in category table.')
     image = db.Column(db.Text(16777216), comment='image for a food')
     calories = db.Column(db.FLOAT)
     fat = db.Column(db.FLOAT)
     carbohydrate = db.Column(db.FLOAT)
     protein = db.Column(db.FLOAT)
-    cholesterol = db.Column(db.FLOAT)
     cellulose = db.Column(db.FLOAT)
     ratioP = db.Column(db.FLOAT)
     ratioCH = db.Column(db.FLOAT)
     ratioF = db.Column(db.FLOAT)
 
-    def __init__(self, name, category, calories, fat, carbohydrate, protein, cholesterol, cellulose, image=''):
+    def __init__(self, name, category, calories, fat, carbohydrate, protein, cellulose, image='', cnName=None):
+        self.cnName = cnName
         self.name = name
         self.category = category
         self.image = image
@@ -26,7 +27,6 @@ class Food(db.Model):
         self.fat = fat
         self.carbohydrate = carbohydrate
         self.protein = protein
-        self.cholesterol = cholesterol
         self.cellulose = cellulose
         accumulatedWeight = protein + carbohydrate + fat
         self.ratioP = round(protein / accumulatedWeight, 3)
@@ -54,8 +54,8 @@ class Food(db.Model):
         return Food.query.filter(Food.name.like('%' + name + '%')).all()
 
     def toDict(self):
-        return {'id': self.id, 'name': self.name, 'category': self.category, 'img': self.image,
+        return {'id': self.id, 'name': self.name, 'cnName': self.cnName, 'category': self.category, 'img': self.image,
                 'calories': self.calories, 'fat': self.fat, 'carbohydrate': self.carbohydrate, 'protein': self.protein,
-                'cholesterol': self.cholesterol, 'cellulose': self.cellulose,
+                'cellulose': self.cellulose,
                 'ratioP': self.ratioP, 'ratioCH': self.ratioCH, 'ratioF': self.ratioF
                 }
