3328e946cf7b7855a12522ba231b946c17b80757
Conn_Lost <hi.imhpc@outlook.com>
2021-05-01 15:09:31 +0800

    db

--- api/db/Category.py
+++ api/db/Category.py
@@ -7,7 +7,20 @@ class Category(db.Model):
     id = db.Column(db.INTEGER, primary_key=True, unique=True, nullable=False, autoincrement=True)
     name = db.Column(db.VARCHAR(256), nullable=False)
     cnName = db.Column(db.VARCHAR(256))
+    desc = db.Column(db.Text(16383))
+    cnDesc = db.Column(db.Text(16383))
 
-    def __init__(self, name, cnName=None):
+    def __init__(self, name, cnName=None, desc=None, cnDesc=None):
         self.cnName = cnName
         self.name = name
+        self.desc = desc
+        self.cnDesc = cnDesc
+
+    def toDict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'cnName': self.cnName,
+            'desc': self.desc,
+            'cnDesc': self.cnDesc
+        }
