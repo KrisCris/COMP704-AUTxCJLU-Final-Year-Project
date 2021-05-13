4f3059602750687499c38891b37a82888b8ac618
yun_cn <zzwyxl@163.com>
2021-05-14 02:48:02 +0800

    计划类型的问题修复

--- fore_end/lib/MyTool/Plan.dart
+++ fore_end/lib/MyTool/Plan.dart
@@ -442,7 +442,7 @@ class BuildMusclePlan extends Plan {
     double dailyProteinUpperLimit,
     double dailyProteinLowerLimit,
   ) : super._internal(
-            2,
+            3,
             id,
             startTime,
             endTime,
@@ -466,7 +466,7 @@ class BuildMusclePlan extends Plan {
 
   @override
   String getPlanType() {
-    return Plan.planTypes[2];
+    return Plan.planTypes[3];
   }
 
   @override
@@ -583,7 +583,7 @@ class MaintainPlan extends Plan {
     double dailyProteinUpperLimit,
     double dailyProteinLowerLimit,
   ) : super._internal(
-            3,
+            2,
             id,
             startTime,
             endTime,
@@ -603,7 +603,7 @@ class MaintainPlan extends Plan {
 
   @override
   String getPlanType() {
-    return Plan.planTypes[3];
+    return Plan.planTypes[2];
   }
 
   @override
